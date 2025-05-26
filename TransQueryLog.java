9d0fc6a21aecee5fe97bdc81a0278deeb8352aa5
AnamikaDebi <anamikadabi2@gmail.com>
2025-05-26 22:50:57 +0530

    Update TransQueryLog.java

--- TransQueryLog.java
+++ TransQueryLog.java
@@ -1,61 +1,37 @@
 package com.pack;
 
-public class TransactionLookupRecord {
+import java.io.Serializable;
 
-	private Long cardId;
-	private Double ucl;
-	private Integer postcode;
-	private String transactionDate;
-	private Integer score;
-	
-	public TransactionLookupRecord(Long cardId)
-	{
-		this.cardId = cardId;
-	}
-	
-	public Long getCardId() {
-		return cardId;
-	}
+public class TransactionLookupRecord implements Serializable {
+    private static final long serialVersionUID = 1L;
 
-	public void setCardId(Long cardId) {
-		this.cardId = cardId;
-	}
-	
-	public Double getUcl() {
-		return ucl;
-	}
+    private Long cardId;
+    private Double ucl;
+    private Integer postcode;
+    private String transactionDate;
+    private Integer score;
 
-	public void setUcl(Double ucl) {
-		this.ucl = ucl;
-	}
+    public TransactionLookupRecord(Long cardId) {
+        this.cardId = cardId;
+    }
 
-	public Integer getPostcode() {
-		return postcode;
-	}
+    public Long getCardId() { return cardId; }
+    public void setCardId(Long cardId) { this.cardId = cardId; }
 
-	public void setPostcode(Integer postcode) {
-		this.postcode = postcode;
-	}
+    public Double getUcl() { return ucl; }
+    public void setUcl(Double ucl) { this.ucl = ucl; }
 
-	public String getTransactionDate() {
-		return transactionDate;
-	}
+    public Integer getPostcode() { return postcode; }
+    public void setPostcode(Integer postcode) { this.postcode = postcode; }
 
-	public void setTransactionDate(String transactionDate) {
-		this.transactionDate = transactionDate;
-	}
+    public String getTransactionDate() { return transactionDate; }
+    public void setTransactionDate(String transactionDate) { this.transactionDate = transactionDate; }
 
-	public Integer getScore() {
-		return score;
-	}
+    public Integer getScore() { return score; }
+    public void setScore(Integer score) { this.score = score; }
 
-	public void setScore(Integer score) {
-		this.score = score;
-	}
-	
-	@Override
-	public String toString()
-	{
-		return this.cardId + "," + this.ucl + "," + this.postcode + "," + this.score + "," + this.transactionDate;
-	}
+    @Override
+    public String toString() {
+        return String.format("%d,%.2f,%d,%d,%s", cardId, ucl, postcode, score, transactionDate);
+    }
 }
